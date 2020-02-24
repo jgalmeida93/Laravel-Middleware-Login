e62ee087e3351c9810f554442cb9898a9c32e6d0
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-24 12:09:09 -0300

    :construction: Login simples através de middleware

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,31 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos', 'ProdutoControlador@index');
+
+Route::post('/login', function(Request $req) {
+
+    $login_ok = false;
+
+    switch($req->input('user')) {
+        case 'joao':
+            $login_ok = $req->input('passwd') === "senhajoao";
+        break;
+        case 'marcos':
+            $login_ok = $req->input('passwd') === "senhamarcos";
+        break;
+        case 'default':
+            $login_ok = false;
+    }
+    if($login_ok) {
+        return response('Login ok', 200);
+    } else {
+        return response('Erro no login', 404);
+    }
+});
